7e3ecfe0fdca6da15db09f49e1c7b83284672406
leathej1 <james.leatherman@gmail.com>
2019-01-05 13:53:08 -0700

    Have you tried turning it off and on again?

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,10 +1,10 @@
 package com.galvanize;
 
-public class application {
+public class Application {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello " + args[0]);
+        System.out.println("Hello world!");
 
     }
 }
--- src/main/java/com/galvanize/Program.java
+++ src/main/java/com/galvanize/Program.java
@@ -1,4 +1,10 @@
 package com.galvanize;
 
 public class Program {
+
+    public static void main(String[] args) {
+
+        System.out.println("It works!!");
+
+    }
 }
